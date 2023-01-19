4992488c9ff0c02869683b2f4674441abfdc29f1
Shubham <shubhamlahase@hmail.com>
2023-01-19 18:24:16 +0530

    added two schema models for users and post

---
+++ models/posts.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema =  new Schema({
+  title : {type: String, require:true},
+  body : {type: String},
+  image :{type: String},
+  user : {type : String, ref:"users"}
+});
+
+const Posts = mongoose.model('blogs', PostSchema);
+
+module.exports = Posts;
