34aa77c6a87534451694878d7f1e5c85cd90bead
Shubham <shubhamlahase@hmail.com>
2023-01-19 20:27:39 +0530

    added jwt authorization to the routes

--- index.js
+++ index.js
@@ -7,6 +7,38 @@ const bodyparser = require("body-parser");
 const app = express();
 app.use(bodyparser.json());
 
+app.use('/api/v1/posts', (req, res, next) => {
+    // console.log(req.headers.authorization)
+    if (req.headers.authorization) {
+        const token = req.headers.authorization;
+        // console.log(token)
+        Jwt.verify(token, secret, function (err, decoded) {
 
+            // console.log(secret)
+            if (err) {
+                return res.status(403).json({
+                    status: "failed",
+                    message: "Not a valid token"
+                })
+            }
+            else {
+                req.user =  decoded.data
+                console.log(decoded.data);
+                next();
+            }
+        })
+    }
+    else {
+        res.status(403).json({
+            status: "Failed",
+            message: "Not authenticated user"
+        })
+    }
+    // console.log(token)
+
+})
+
+app.use('/api/v1/users', loginRoutes)
+app.use('/api/v1/', postsRoutes)
 
 app.listen(port, () => console.log(`Server Running on ${port}`));
